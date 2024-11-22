fe5f43167c9deee58e6cacc0cafd730f3cbd2559
Shivamprajapati91 <139732922+Shivamprajapati91@users.noreply.github.com>
2024-11-22 15:06:38 +0530

    Update index.js

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -6,39 +6,48 @@ const backendDomain = process.env.REACT_APP_BACKEND_URL//"http://localhost:8080"
 const SummaryApi={
     signUp: {
        url: `${backendDomain}/api/signup`,
-       method: "post"
+       method: "post",
+         mode: 'no-cors'
     },
     signin: {
         url: `${backendDomain}/api/signin`,
-        method: "post"
+        method: "post",
+         mode: 'no-cors'
     },
     current_user:{
         url: `${backendDomain}/api/user-details`,
-        method: "get"
+        method: "get",
+         mode: 'no-cors'
     },
     logout_user:{
        url: `${backendDomain}/api/user-logout`,
-       method: 'get'
+       method: 'get',
+         mode: 'no-cors'
     },
     allUser: {
         url: `${backendDomain}/api/all-users`,
-        method: 'get'
+        method: 'get',
+         mode: 'no-cors'
     },
     updateUser: {
         url: `${backendDomain}/api/update-user`,
-        method: 'post'
+        method: 'post',
+         mode: 'no-cors'
     },
     uploadProduct: {
         url: `${backendDomain}/api/upload-product`,
-        method: 'post'
+        method: 'post',
+         mode: 'no-cors'
     },
     allProduct: {
         url: `${backendDomain}/api/get-product`,
-        method: 'get'
+        method: 'get',
+         mode: 'no-cors'
     },
     updateProduct: {
         url: `${backendDomain}/api/update-product`,
-        method: 'post'
+        method: 'post',
+         mode: 'no-cors'
     },
     categoryProduct: {
         url: `${backendDomain}/api/get-categoryProduct`,
@@ -47,47 +56,58 @@ const SummaryApi={
     },
     categoryWiseProduct: {
          url: `${backendDomain}/api/category-product`,
-        method: 'post'
+        method: 'post',
+         mode: 'no-cors'
     },
     productDetails: {
         url: `${backendDomain}/api/product-details`,
-        method: 'post'
+        method: 'post',
+         mode: 'no-cors'
     },
     addToCartProduct: {
         url : `${backendDomain}/api/addtocart`,
-        method: 'post'
+        method: 'post',
+         mode: 'no-cors'
     },
     addToCartProductCount: {
         url : `${backendDomain}/api/countAddToCartProduct`,
-        method: 'get'
+        method: 'get',
+         mode: 'no-cors'
     },
     addToCartProductView: {
         url:  `${backendDomain}/api/view-cart-product`,
-        method: 'get'
+        method: 'get',
+         mode: 'no-cors'
     },
     updateCartProduct:{
          url:  `${backendDomain}/api/update-cart-product`,
-         method: 'post'
+         method: 'post',
+         mode: 'no-cors'
     },
     deleteCartProduct: {
            url:  `${backendDomain}/api/delete-cart-product`,
-            method: 'post'
+            method: 'post',
+         mode: 'no-cors'
     },
     searchProduct : {
            url:  `${backendDomain}/api/search`,
-            method: 'get'
+            method: 'get',
+         mode: 'no-cors'
     },
     filterProduct: {
           url:  `${backendDomain}/api/filter-product`,
-            method: 'post'
+            method: 'post',
+         mode: 'no-cors'
     },
     payment: {
         url:  `${backendDomain}/api/checkout`,
-            method: 'post'
+            method: 'post',
+         mode: 'no-cors'
     },
     getOrder: {
          url:  `${backendDomain}/api/order-list`,
-            method: 'get'
+            method: 'get',
+         mode: 'no-cors'
     }
     
 }
